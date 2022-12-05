6c673cfeed85ce72b28730765ef3643a1be31a35
Miguel A. Cádiz <macevedoinf@gmail.com>
2022-12-05 20:56:21 -0300

    Calculating max showing agents and fixing some styles

--- src/Components/AgentList/actions.ts
+++ src/Components/AgentList/actions.ts
@@ -9,3 +9,12 @@ export const getAgents = async () => {
 export const orderAgents = (agents: TAgent[]): TAgent[] => {
     return agents.sort((a, b) => a.displayName >= b.displayName ? 1 : -1);
 }
+
+export const calculateMaxAgents = (containerSize: number) => {
+    const agentCardWidth = 108;
+
+    const calculatedAgents = Math.floor(containerSize/agentCardWidth);
+
+    // We must always keep the selected agent to the center, thus we need always an odd quantity of agents
+    return calculatedAgents % 2 === 0 ? calculatedAgents - 1 : calculatedAgents;
+}
--- src/Components/AgentList/index.tsx
+++ src/Components/AgentList/index.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
+import { FC, useRef, useState, useEffect } from "react";
 import AgentCard from "../AgentCard";
+import { calculateMaxAgents } from "./actions";
 
 import "./agentsList.css";
 import { AgentListProps } from "./types";
@@ -9,23 +10,35 @@ const AgentList: FC<AgentListProps> = ({
   selectedAgent,
   onSelectAgent,
 }) => {
+  const [maxNumberOfAgents, setMaxNumberOfAgents] = useState(0);
+  const agentsListRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (agentsListRef.current) {
+      const containerSize = agentsListRef.current.parentElement?.clientWidth;
+
+      const calculatedMaxAgents = calculateMaxAgents(containerSize ?? 0);
+
+      console.log(containerSize, calculatedMaxAgents);
+
+      setMaxNumberOfAgents(calculatedMaxAgents);
+    }
+  }, [agents, agentsListRef]);
+
   return (
-    <>
-      <div className="agents-list">
-        {agents.map((agent) => {
-          const isSelected =
-            !!selectedAgent && selectedAgent.uuid === agent.uuid;
-          return (
-            <AgentCard
-              key={agent.uuid}
-              agent={agent}
-              onClick={onSelectAgent}
-              isSelected={isSelected}
-            />
-          );
-        })}
-      </div>
-    </>
+    <div className="agents-list" ref={agentsListRef}>
+      {agents.slice(0, maxNumberOfAgents).map((agent) => {
+        const isSelected = !!selectedAgent && selectedAgent.uuid === agent.uuid;
+        return (
+          <AgentCard
+            key={agent.uuid}
+            agent={agent}
+            onClick={onSelectAgent}
+            isSelected={isSelected}
+          />
+        );
+      })}
+    </div>
   );
 };
 
