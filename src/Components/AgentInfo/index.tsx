d140645e0897b8248f33a88713ee698c8ee76f96
Miguel Acevedo Cádiz <macevedoinf@gmail.com>
2023-07-09 21:12:14 -0400

    Adding portrait animation

--- src/Components/AgentInfo/index.tsx
+++ src/Components/AgentInfo/index.tsx
@@ -1,10 +1,22 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { AgentInfoProps } from "./types";
 
 import "./agentInfo.css";
 import AgentDetails from "./AgentDetails";
 
 const AgentInfo: FC<AgentInfoProps> = ({ agent }) => {
+  const [shouldAnimate, setShouldAnimate] = useState(true);
+
+  useEffect(() => {
+    setShouldAnimate(false);
+  }, [agent]);
+
+  useEffect(() => {
+    if (!shouldAnimate) {
+      setShouldAnimate(true);
+    }
+  }, [shouldAnimate]);
+
   return (
     <>
       <AgentDetails
@@ -13,12 +25,19 @@ const AgentInfo: FC<AgentInfoProps> = ({ agent }) => {
         abilities={agent.abilities}
         role={agent.role}
       />
-      <div
-        className="agent-info"
-        style={{
-          backgroundImage: `url('${agent.fullPortrait}')`,
-        }}
-      />
+      {shouldAnimate && (
+        <div className="agent-info animate">
+          <img
+            alt={agent.displayName}
+            className="agent-image"
+            src={agent.fullPortrait}
+            style={{
+              height: "100%",
+              color: `#${agent.backgroundGradientColors[0]}`,
+            }}
+          />
+        </div>
+      )}
       <div
         className="agent-info-agent-name-background"
         style={{
